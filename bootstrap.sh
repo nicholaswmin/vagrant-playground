05a10aaa0fc4d4797854d77a3a6b39f1d20fc6cc
Nicholas Kyriakides <nicholaswmin@Nicholass-iMac.local>
2017-07-10 08:13:00 +0300

    installed mssql-tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,13 @@ sudo apt-get update
 sudo apt-get install -y mssql-server
 
 sudo SA_PASSWORD='vagrantT321' bash -c '/opt/mssql/bin/mssql-conf setup accept-eula'
+
+# SQL Server Tools
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+sudo apt-get update
+sudo ACCEPT_EULA='Y' bash -c 'apt-get install -y mssql-tools unixodbc-dev'
+
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
